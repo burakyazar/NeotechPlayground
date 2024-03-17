c8a6651795b413f603731970fdc0d4f55e7e7052
burakyazar <fatsa@hotmail.com.tr>
2024-03-16 17:22:08 -0700

    added another setUp method with url method.

--- src/Homework6/Task1.java
+++ src/Homework6/Task1.java
@@ -1,7 +1,6 @@
 package Homework6;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 
 import utils.BaseClass;
 
--- src/utils/BaseClass.java
+++ src/utils/BaseClass.java
@@ -1,6 +1,5 @@
 package utils;
 
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -34,6 +33,32 @@ public class BaseClass {
 		driver.get(website);
 	}
 
+	
+	public static void setUp(String url) throws InterruptedException 
+	{
+		ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+		String browser = ConfigsReader.getProperty("browser");
+
+		switch (browser.toLowerCase()) {
+		case "chrome": 
+			driver = new ChromeDriver();
+			break;
+		
+		case "firefox": 
+			driver = new FirefoxDriver();
+			break;
+		
+		default:
+			driver = new ChromeDriver();
+			break;
+		}
+
+		Thread.sleep(2000);
+		//driver.manage().window().setPosition(new Point(100, 100));
+		driver.manage().window().maximize();
+		
+		driver.get(url);
+	}
 	public static void tearDown() {
 
 		if (driver != null) {
